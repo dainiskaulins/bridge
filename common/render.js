074ca46dc5d662a4c3c5c41edc1c1c4aca322c81
dainiskaulins <dainis.kaulins@gmail.com>
2026-07-01 16:56:21 +0300

    Update render.js

--- common/render.js
+++ common/render.js
@@ -1,11 +1,16 @@
 // common/render.js
 // centrālais pārzīmētājs
 
-function renderAll() {
-  renderTitlePanel();
-  renderButtonPanel();
-  renderTablePanel();
-  renderLogPanel();
-}
+//function renderAll() {
+//  renderTitlePanel();
+//  renderButtonPanel();
+//  renderTablePanel();
+//  renderLogPanel();
+//}
 
+function render() {
+  document.getElementById("logPanel").textContent =
+    "FACTS\n" +
+    JSON.stringify(facts, null, 2);
+} 
 console.log("common/render.js ielādēts");
