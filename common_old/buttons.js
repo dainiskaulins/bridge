4da59b992ed1cbc1df6b05e35568650d172b3871
dainiskaulins <dainis.kaulins@gmail.com>
2026-07-01 15:31:13 +0300

    Create buttons.js

---
+++ common_old/buttons.js
@@ -0,0 +1,10 @@
+// common/buttons.js
+
+function pogasKrasa(id, klase) {
+  const poga = document.getElementById(id);
+
+  poga.classList.remove("poga-zala", "poga-dzeltena", "poga-sarkana", "poga-peleka");
+  poga.classList.add(klase);
+}
+
+console.log("buttons.js ielādēts");
