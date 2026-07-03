e1debe40e35cac5839d9aa580092a15f18835fe2
dainiskaulins <dainis.kaulins@gmail.com>
2026-07-03 16:28:03 +0300

    Add comments to common/actions.js
    Add comments explaining the purpose of actions in actions.js

---
+++ common/actions.js
@@ -0,0 +1,5 @@
+// common/actions.js
+//
+// Darbības, kas maina FACTS.
+// Šeit pievieno solījumu, izspēlē kārti, ģenerē izdali, anulē pēdējo darbību.
+// Pirms izmaiņām darbības izmanto rules.js pārbaudes.
