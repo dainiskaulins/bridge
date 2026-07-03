dfb1d9078e93fabb47afb0e4a52c27e7ccf6872f
dainiskaulins <dainis.kaulins@gmail.com>
2026-07-03 16:42:25 +0300

    Add comments to utils.js for better clarity
    Added comments to explain the purpose of the utils.js file.

--- common/utils.js
+++ common/utils.js
@@ -1,4 +1,8 @@
 // common/utils.js
+// 
+// Vispārīgas palīgfunkcijas.
+// Mazas funkcijas, kas noder vairākās vietās: kopēšana, nejauša izvēle, teksta formatēšana u.c.
+// Šeit neliek specifiskus bridža noteikumus.
 
 function log(message) {
     console.log(message);
