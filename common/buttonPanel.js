0988a7e1769bd6b23d582100f5ca888e4351e7c8
dainiskaulins <dainis.kaulins@gmail.com>
2026-07-05 14:00:55 +0300

    Add logging for button panel rendering

--- common/buttonPanel.js
+++ common/buttonPanel.js
@@ -156,6 +156,7 @@ function izsauktPogasFunkciju(poga) {
 //=========================================
 function renderButtonPanel(buttonSet) {
   console.log("sākam renderButtonPanel");
+  console.log("facts.status: " + facts.status);
   console.log(buttonSet.nosaukums);
   console.log(buttonSet);
   
