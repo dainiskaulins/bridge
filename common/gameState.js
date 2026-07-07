70ccbfaf089dc00d2a3960e24b39695127443076
dainiskaulins <dainis.kaulins@gmail.com>
2026-07-07 20:10:13 +0300

    Fix formatting in gameState.js
    Added a comma for consistency in the play object.

--- common/gameState.js
+++ common/gameState.js
@@ -54,7 +54,7 @@ let gameState = {
 
         trick: null,          // izspēles stiķa numurs 
         position: null,       // 1..4, kura kārts stiķī
-        requiredSuit: null    // "C","D","H","S" vai null, ja pirmais stiķīs
+        requiredSuit: null,    // "C","D","H","S" vai null, ja pirmais stiķīs
         // leader: null,
         currentPlayer: null   // "S", "W", "N", "E"
         // requiredSuit: null,
